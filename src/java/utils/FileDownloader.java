d281e810b928dfc398e2074c5b37a1a3a5cc37c4
Mário Cao <kreativermario@gmail.com>
2023-04-10 17:41:08 +0100

    fix: downloadFile agora permite que o utilizador possa escolher o nome do ficheiro
    Signed-off-by: Mário Cao <kreativermario@gmail.com>

--- src/java/utils/FileDownloader.java
+++ src/java/utils/FileDownloader.java
@@ -5,85 +5,85 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Files;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 public class FileDownloader {
     /**
-     * Downloads a file from a remote URL and saves it to a local file.
+     * Faz download de um ficheiro CSV ou JSON dado um link URL e guarda num ficheiro local
      *
-     * @param remoteUrl the URL of the remote file to download
-     * @return the local file that the remote file was saved to
-     * @throws IOException if an error occurs during the download or saving process
+     * @param remoteUrl o URL do ficheiro remoto
+     * @return o ficheiro local caso seja feito com sucesso, null se não tiver sucesso
+     * @throws IOException caso ocorrra um erro durante o processo de download ou de guardar o ficheiro
      */
     public static File downloadFile(URL remoteUrl) throws IOException {
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         int result = fileChooser.showSaveDialog(null);
         if (result != JFileChooser.APPROVE_OPTION) {
-            return null; // User cancelled the dialog
+            return null; // utilizador cancela o processo
         }
 
         File selectedDirectory = fileChooser.getSelectedFile();
-        String fileName = getFileName(remoteUrl);
+        String fileName = JOptionPane.showInputDialog(null,
+                "Introduza o nome do ficheiro (sem a extensão):");
+        if (fileName == null || fileName.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Por favor escolha um nome válido!",
+                    "Erro", JOptionPane.ERROR_MESSAGE);
+            return null;
+        }
+
+        // Adiciona a extensão apropriada ao nome do arquivo
+        String fileExtension = getFileExtension(remoteUrl);
+        fileName += "." + fileExtension;
         File localFile = new File(selectedDirectory, fileName);
 
         FileUtils.copyURLToFile(remoteUrl, localFile);
-        String fileExtension = getFileExtension(localFile);
-        if (!fileExtension.equalsIgnoreCase("csv") && !fileExtension.equalsIgnoreCase("json")) {
-            JOptionPane.showMessageDialog(null, "The selected file is not a CSV or JSON file.",
-                    "Error", JOptionPane.ERROR_MESSAGE);
+        if (!fileExtension.equalsIgnoreCase("csv") &&
+                !fileExtension.equalsIgnoreCase("json")) {
+
+            JOptionPane.showMessageDialog(null,
+                    "O arquivo selecionado não é um arquivo CSV ou JSON.",
+                    "Erro", JOptionPane.ERROR_MESSAGE);
             return null;
+
         }
 
         return localFile;
     }
 
-
     /**
-     * Prompts the user to enter a remote URL and downloads the corresponding file.
+     * Pede ao utilizador para inserir um URL remoto e faz download do ficheiro correspondente.
      *
-     * @return the local file that the remote file was saved to, or null if the user cancelled the dialog
+     * @return o ficheiro local onde o ficheiro remoto foi salvo, ou null se o utilizador cancelar ou ocorrer erro.
      */
     public static File downloadRemoteFile() {
-        String url = JOptionPane.showInputDialog(null, "Enter the URL of the remote file:");
+        String url = JOptionPane.showInputDialog(null, "Introduza o URL do ficheiro remoto:");
         if (url == null || url.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Please enter a valid URL.", "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Por favor introduza um URL válido.",
+                    "Erro", JOptionPane.ERROR_MESSAGE);
             return null;
         }
 
         try {
             return downloadFile(new URL(url));
         } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "An error occurred while downloading the file.",
-                    "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Ocorreu um erro ao fazer download do ficheiro.",
+                    "Erro", JOptionPane.ERROR_MESSAGE);
             return null;
         }
     }
 
     /**
-     * Returns the file extension of a file.
+     * Retorna a extensão de um arquivo.
      *
-     * @param file the file to get the extension from
-     * @return the extension of the file, or null if the file has no extension
+     * @param file o ficheiro para obter a extensão do ficheiro
+     * @return a extensão do arquivo, ou null se o arquivo não tiver extensão
      */
-    private static String getFileExtension(File file) {
-        String name = file.getName();
+    private static String getFileExtension(URL file) {
+        String name = file.getPath();
         int dotIndex = name.lastIndexOf(".");
         return (dotIndex == -1) ? null : name.substring(dotIndex + 1);
-    }
-
-    /**
-     * Returns the file name of a URL.
-     *
-     * @param url the URL to get the file name from
-     * @return the file name of the URL
-     */
-    private static String getFileName(URL url) {
-        String path = url.getPath();
-        int lastIndexOfSlash = path.lastIndexOf("/");
-        return (lastIndexOfSlash == -1) ? path : path.substring(lastIndexOfSlash + 1);
     }
 
 }
