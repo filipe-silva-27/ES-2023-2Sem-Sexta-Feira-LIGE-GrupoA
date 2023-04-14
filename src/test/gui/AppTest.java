0e0adff7355f290aef5a1e4a220fa1c97fdd4ef7
Mário Cao <kreativermario@gmail.com>
2023-04-14 20:37:25 +0100

    feat: adicionado caso testes para App.java
    Signed-off-by: Mário Cao <kreativermario@gmail.com>

--- src/test/gui/AppTest.java
+++ src/test/gui/AppTest.java
@@ -1,7 +1,77 @@
 package gui;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
-public class AppTest extends TestCase {
-/**/
+import models.Schedule;
+import org.junit.*;
+import org.mockito.*;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.time.DayOfWeek;
+import java.util.EnumMap;
+import javax.swing.*;
+
+public class AppTest {
+
+    private App app;
+    private Schedule schedule;
+
+    @Before
+    void setUp() {
+        app = Mockito.spy(new App());
+        schedule = new Schedule("Test Schedule", 7);
+        app.setSchedule(schedule);
+    }
+
+    @After
+    void tearDown() {
+        //TODO To be implemented
+    }
+
+    /**
+     * Description: Test method for shipAt.
+     */
+    @Test
+    void testMain() {
+        // Verify that the main method runs without exceptions
+        App.main(new String[] {});
+    }
+
+    /**
+     * Description: Test method for shipAt.
+     */
+    @Test
+    void testGetFrame() {
+        //TODO to implement
+        fail("Not implemented yet");
+    }
+
+    /**
+     * Description: Test method for shipAt.
+     */
+    @Test
+    void testGetMainPanel() {
+        //TODO to implement
+        fail("Not implemented yet");
+    }
+
+    /**
+     * Description: Test method for getSchedule.
+     */
+    @Test
+    void testGetSchedule() {
+        Schedule s = app.getSchedule();
+        assertNotNull(s);
+        assertEquals(s, schedule);
+    }
+
+    /**
+     * Description: Test method for setSchedule.
+     */
+    @Test
+    void testSetSchedule() {
+        Schedule s = new Schedule("Another Test Schedule", 14);
+        app.setSchedule(s);
+        assertEquals(s, app.getSchedule());
+    }
 }
