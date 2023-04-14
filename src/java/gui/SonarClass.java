b67cb23de1a37e8a3d0a981ef1bb4816d0dcfc7b
Ruisth23 <125674837+Ruisth23@users.noreply.github.com>
2023-04-14 16:20:39 +0100

    test: teste de análise de SonarCloud 2
    Signed-off-by: Ruisth23 <125674837+Ruisth23@users.noreply.github.com>

--- src/java/gui/SonarClass.java
+++ src/java/gui/SonarClass.java
@@ -3,6 +3,6 @@ package gui;
 public class SonarClass {
 
     /*
-    Esta é uma classe de teste para testar o SonarCloud!!!
+    Esta é uma classe de teste para testar o SonarCloud!!!!!!
      */
 }
